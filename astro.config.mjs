2b37164a21b17234ff58505b39468f672e881f2c
Nirmit <nirmitj0303@gmail.com>
2025-06-18 00:10:01 +0530

    Revert "migrate to nirmitjoshi.github.io"
    This reverts commit dc4c3ca8cc246c82bad47c53b46cb5fb92a0f62d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://nirmitjoshi.github.io",
+  site: "https://nirmitjoshi.tech",
   markdown: {
     shikiConfig: {
       theme: "vitesse-dark",
